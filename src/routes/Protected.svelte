7235455af1b1573548db67d15000bf153edf941a
Rene Dellefont <renedellefont@gmail.com>
2019-09-24 18:01:56 -0400

    Add Protected Route functionality

--- src/routes/Protected.svelte
+++ src/routes/Protected.svelte
@@ -1,5 +1,25 @@
+<script>
+  import { navigate } from 'svelte-routing'
+  import Swal from 'sweetalert2'
+  import { user } from '../store.js'
+
+  function handlePrivateRoute() {
+    navigate('/', { replace: true })
+
+    Swal.fire({
+      title: 'You are not authenticated',
+      text: 'Please log in or sign up to view this page',
+      type: 'error',
+      allowOutsideClick: false,
+      confirmButtonText: 'Will do!',
+    })
+  }
+</script>
+
 <svelte:head>
   <title>Protected Route</title>
 </svelte:head>
 
-<h1>Protected</h1>
+{#if $user && $user.username}
+  <h1>Protected</h1>
+{:else}{handlePrivateRoute()}{/if}
