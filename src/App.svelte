a54921e77b9a547364c93ef3c38d924ffbf8791f
Rene Dellefont <renedellefont@gmail.com>
2019-09-24 14:38:20 -0400

    Add basic example structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,96 @@
 <script>
-  export let name
+  import { Router, Route, Link, navigate } from 'svelte-routing'
+  import Home from './routes/Home.svelte'
+  import Public from './routes/Public.svelte'
+  import Protected from './routes/Protected.svelte'
+  import NotFound from './routes/NotFound.svelte'
+
+  let isLoggedIn
+  let username
 </script>
 
 <style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    color: #2c3e50;
+  }
+  :global(a, a:visited) {
+    color: #ff3e00;
+  }
+  main {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  img {
+    display: block;
+    width: 200px;
+    max-width: 100%;
+    height: auto;
+    order: -1;
+  }
+  ul {
+    display: flex;
+    flex-direction: row;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  ul > * + * {
+    margin-left: 1rem;
+  }
+  li {
+  }
   h1 {
-    color: purple;
+    font-size: 1.2rem;
   }
 </style>
 
-<h1>Hello {name}!</h1>
+<main>
+  <h1>Netlify Identity Demo with Svelte</h1>
+
+  <img alt="Svelte logo" src="logo.svg" />
+
+  {#if isLoggedIn}
+    <div>
+      <p>Hello {username}</p>
+      <p>
+        <button on:click={() => console.log('logout')}>Log Out</button>
+      </p>
+    </div>
+  {:else}
+    <div>
+      <p>You are not logged in.</p>
+      <p>
+        <button on:click={() => console.log('login')}>Log In</button>
+        <button on:click={() => console.log('signup')}>Sign Up</button>
+      </p>
+    </div>
+  {/if}
+
+  <Router>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/public">Public</Link>
+      </li>
+      <li>
+        <Link to="/protected">Protected</Link>
+      </li>
+    </ul>
+    <Route path="/public" component={Public} />
+    <Route path="/protected" component={Protected} />
+    <Route path="/" component={Home} />
+  </Router>
+  <a href="https://github.com/whizjs/netlify-identity-demo-vue">
+    <img
+      style="position: absolute; top: 0; right: 0; border: 0;"
+      src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
+      alt="Fork me on GitHub" />
+  </a>
+</main>
