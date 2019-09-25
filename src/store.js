dbce1d8c823daf72a3351ee0c8a80b1eef4862c4
Rene Dellefont <renedellefont@gmail.com>
2019-09-25 13:10:41 -0400

    Add redirect Store

--- src/store.js
+++ src/store.js
@@ -35,4 +35,18 @@ function createUser() {
   }
 }
 
+function createRedirectURL() {
+  const { subscribe, set } = writable('')
+  return {
+    subscribe,
+    setRedirectURL(url) {
+      set(url)
+    },
+    clearRedirectURL() {
+      set('')
+    },
+  }
+}
+
 export const user = createUser()
+export const redirectURL = createRedirectURL()
