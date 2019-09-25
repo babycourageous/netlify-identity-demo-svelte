87edfe1640da08081c26c17ec751eb87047f836d
Rene Dellefont <renedellefont@gmail.com>
2019-09-25 13:10:29 -0400

    Update user store with gotrue localStorage

--- src/store.js
+++ src/store.js
@@ -1,11 +1,32 @@
 import { writable } from 'svelte/store'
 
 function createUser() {
-  const { subscribe, set } = writable(null)
+  const localUser = JSON.parse(localStorage.getItem('gotrue.user'))
+
+  let u = null
+  if (localUser) {
+    u = {
+      username: localUser.user_metadata.full_name,
+      email: localUser.email,
+      access_token: localUser.token.access_token,
+      expires_at: localUser.token.expires_at,
+      refresh_token: localUser.token.refresh_token,
+      token_type: localUser.token.token_type,
+    }
+  }
+  const { subscribe, set } = writable(u)
 
   return {
     subscribe,
-    login(currentUser) {
+    login(user) {
+      const currentUser = {
+        username: user.user_metadata.full_name,
+        email: user.email,
+        access_token: user.token.access_token,
+        expires_at: user.token.expires_at,
+        refresh_token: user.token.refresh_token,
+        token_type: user.token.token_type,
+      }
       set(currentUser)
     },
     logout() {
